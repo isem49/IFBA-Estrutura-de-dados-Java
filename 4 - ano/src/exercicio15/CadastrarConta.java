c8dda0592051b5ca3944711206c3bc7fca0f3d86
Júnior <56940605+isem49@users.noreply.github.com>
2022-04-05 11:40:41 -0300

    Add files via upload

--- 4 - ano/src/exercicio15/CadastrarConta.java
+++ 4 - ano/src/exercicio15/CadastrarConta.java
@@ -1,13 +1,14 @@
-package exercicio15;
+package exercicio;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class CadastrarConta {
 
-	Conta vetor[] = new Conta[3];
+	Conta vetor[] = new Conta[10];
 	Conta c = new Conta();
 	Scanner in = new Scanner(System.in);
+	int aux = 0;
 
 	public void procuraConta() {
 
@@ -15,7 +16,7 @@ public class CadastrarConta {
 		System.out.print("Digite o nome que você quer procurar: ");
 		String nome = in.next();
 
-		for (int cont = 0; cont < vetor.length; cont++) {
+		for (int cont = 0; cont < aux; cont++) {
 			if (nome.equals(vetor[cont].getNome())) {
 
 				System.out.println();
@@ -26,14 +27,14 @@ public class CadastrarConta {
 				System.out.println("---------------------------------");
 
 			}
-
+			
 		}
 	}
 
 	public void listarConta() {
 
 		System.out.println("Contas Cadastradas: ");
-		for (int cont = 0; cont < vetor.length; cont++) {
+		for (int cont = 0; cont <= 3; cont++) {
 
 			System.out.println();
 			System.out.println("---------------------------------");
@@ -46,16 +47,35 @@ public class CadastrarConta {
 
 	}
 
+	public void addPos() {
+
+		System.out.println("Digite a posição que você quer colocar: ");
+		int pos = in.nextInt();
+
+		for (int cont = aux - 1; cont >= pos; cont--) {
+
+			vetor[cont + 1] = vetor[cont];
+
+		}
+
+		System.out.print("Digite um nome: ");
+		c.setNome(in.next());
+		System.out.print("Digite o valor da conta: ");
+		c.setValor(in.nextInt());
+		vetor[pos] = c;
+
+	}
+
 	public void adicionarConta() {
-		for (int i = 0; i < vetor.length; i++) {
-			
+		for (int i = 0; i < 3; i++) {
+
 			Conta c = new Conta();
 			System.out.print("Digite um nome: ");
 			c.setNome(in.next());
 			System.out.print("Digite o valor da conta: ");
 			c.setValor(in.nextInt());
 			vetor[i] = c;
-			
+			aux = i+1;
 		}
 
 	}
--- 4 - ano/src/exercicio15/Principal.java
+++ 4 - ano/src/exercicio15/Principal.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		CadastroConta cc = new CadastroConta();
+		CadastrarConta cc = new CadastrarConta();
 		Scanner in = new Scanner(System.in);
 
 		cc.adicionarConta();
